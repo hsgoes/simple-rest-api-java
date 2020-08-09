307807b2d903bbeb394d4c0712f7b00fdf91dd95
Hugo Goes <hugo.henkel@hotmail.com>
2020-08-09 18:04:41 -0300

    added jwt authentication to protect the resources

--- src/main/java/com/springapi/expensetrackerapi/resources/UserResource.java
+++ src/main/java/com/springapi/expensetrackerapi/resources/UserResource.java
@@ -1,8 +1,11 @@
 package com.springapi.expensetrackerapi.resources;
 
 
+import com.springapi.expensetrackerapi.Constants;
 import com.springapi.expensetrackerapi.domain.User;
 import com.springapi.expensetrackerapi.services.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,10 +30,8 @@ public class UserResource {
         String email = (String) userMap.get("email");
         String password = (String) userMap.get("password");
         User user = userService.validateUser(email, password);
-        Map<String, String> map = new HashMap<>();
-        map.put("message", "loggedIn successfully");
 
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
     }
 
     @PostMapping("/register")
@@ -40,10 +42,23 @@ public class UserResource {
         String password = (String) userMap.get("password");
 
         User user = userService.registerUser(firstName, lastName, email, password);
-        Map<String, String> map = new HashMap<>();
-        map.put("message", "registered successfully");
+        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
+    }
 
-        return new ResponseEntity<>(map, HttpStatus.OK);
+    private Map<String, String> generateJWTToken(User user){
+        long timestamp = System.currentTimeMillis();
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+                .setIssuedAt(new Date(timestamp))
+                .setExpiration(new Date(timestamp + Constants.TOKEN_VALIDITY))
+                .claim("userId", user.getUserId())
+                .claim("email", user.getEmail())
+                .claim("firstName", user.getFirstName())
+                .claim("lastName", user.getLastName())
+                .compact();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("token", token);
+        return map;
     }
 
 }
